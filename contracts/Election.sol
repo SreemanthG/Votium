6b107053eba605e1a6b4aa728aba3504a13eadb3
SreemanthG <sreemanth2001@gmail.com>
2021-04-16 16:30:47 +0530

    listed candidates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,12 +1,27 @@
 pragma solidity >=0.4.22 <0.8.0;
 
 contract Election {
-    //constructor
 
-    //Read Candidate 
-    string public candidate;
+    //Model a candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    //Store the candidate
+    //Fetch Candidate
+    mapping(uint=> Candidate) public candidates;
+    //Store Candidate Count
+    uint public candidatesCount;
 
     constructor () public{
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate(string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
