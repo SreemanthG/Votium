3db7887b9b2e2612aa493b71151d8cd5755f9907
SreemanthG <sreemanth2001@gmail.com>
2021-04-16 14:59:45 +0530

    1. Smoke Test

---
+++ contracts/Election.sol
@@ -0,0 +1,12 @@
+pragma solidity >=0.4.22 <0.8.0;
+
+contract Election {
+    //constructor
+
+    //Read Candidate 
+    string public candidate;
+
+    constructor () public{
+        candidate = "Candidate 1";
+    }
+}
